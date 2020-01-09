3f39a9af58372a1c227995c097c03f6a6b199146
Inesz <agnieszka-garsta@wp.pl>
2020-01-09 15:41:24 +0100

    decorator (structural) ex1 supplement

--- structural/decorator/src/main/java/ex1/Main.java
+++ structural/decorator/src/main/java/ex1/Main.java
@@ -4,8 +4,20 @@ public class Main {
     public static void main(String[] args) {
         ChristmasTree tree1 = new BubbleLights(new ChristmasTreeImpl());
         ChristmasTree tree2 = new BubbleLights(new Garland(new BubbleLights(new ChristmasTreeImpl())));
-
+        ChristmasTree tree3 = new BubbleLights(new Garland(new ChristmasTreeImpl()));
+        
         System.out.println(tree1.decorate());
         System.out.println(tree2.decorate());
+        System.out.println(tree3.decorate());
     }
 }
+
+/*
+decorate() execute recursion;
+
+Output:
+
+Christmas tree with Bubble Lights
+Christmas tree with Bubble Lights with Garland with Bubble Lights
+Christmas tree with Garland with Bubble Lights
+ */
